d5087a85d79759ce0974cc4d7a01b522592578a9
devYeonjun <alcls0026@gmail.com>
2022-06-03 19:59:12 +1200

    Add routes & navbar css by bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import Staff from "./components/pages/Staff";
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Navbar />
+				<Route path="/" exact={true} component={Home} />
+				<Route path="/staff" exact={true} component={Staff} />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
